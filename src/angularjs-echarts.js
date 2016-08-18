e5346fb6109005d2c03b7b54e9d21fef237ded41
danjarvis <danjarvis@ks2inc.com>
2016-08-18 09:13:00 -0500

    Added test

--- src/angularjs-echarts.js
+++ src/angularjs-echarts.js
@@ -1,4 +1,4 @@
-angular.module('angularjs-echarts.echarts', []) // eslint-disable-line
+angular.module('angularjs-echarts.echarts', [])
 
 .directive('echarts', function () {
   return {
@@ -11,23 +11,23 @@ angular.module('angularjs-echarts.echarts', []) // eslint-disable-line
     link: function (scope, elem, attrs) {
       var updateGraph = function (newValue, oldValue) {
         // Get specified height and width
-        var height = scope.height
-        var width = scope.width
-        if (height === undefined) height = '300px'
-        if (width === undefined) width = '300px'
+        var height = scope.height;
+        var width = scope.width;
+        if (height === undefined) height = '300px';
+        if (width === undefined) width = '300px';
 
         // Set a default width and height
-        var chartElement = document.getElementById(attrs.id)
-        chartElement.style.width = width
-        chartElement.style.height = height
+        var chartElement = document.getElementById(attrs.id);
+        chartElement.style.width = width;
+        chartElement.style.height = height;
 
         // Create chart
-        var chart = echarts.init(document.getElementById(attrs.id)) // eslint-disable-line
-        chart.setOption(scope.options)
-      }
-      scope.$watch('height', updateGraph)
-      scope.$watch('width', updateGraph)
-      scope.$watch('options.series[0].data', updateGraph, true)
+        var chart = echarts.init(document.getElementById(attrs.id));
+        chart.setOption(scope.options);
+      };
+      scope.$watch('height', updateGraph);
+      scope.$watch('width', updateGraph);
+      scope.$watch('options.series[0].data', updateGraph, true);
     }
-  }
-})
+  };
+});
